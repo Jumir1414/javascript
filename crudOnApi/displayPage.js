c88c330d4ee3df0b92bb80eebf29e83e859c0e62
Jumir1414 <72116725+Jumir1414@users.noreply.github.com>
2023-03-02 11:46:41 +0545

    ...

--- crudOnApi/displayPage.js
+++ crudOnApi/displayPage.js
@@ -1,11 +1,6 @@
 const List = document.getElementById("List");
 const Loader = document.getElementById("loader");
 
-// var myHeaders = new Headers();
-// myHeaders.append(
-//   "Authorization",
-//   "Bearer c1d4b2596391a25a380f607f473599192aa00c6a4c903f388301e7faad33d94f"
-// );
 const requestOptions = (i) => {
   var requestOptions = {
     method: i,
