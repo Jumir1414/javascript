985643beeece118d70d843ff3a71ed1fa44a3275
Jumir1414 <72116725+Jumir1414@users.noreply.github.com>
2023-03-02 21:29:24 +0545

    currencyConverter created

---
+++ currencyConvert/currencyConverter.js
@@ -0,0 +1,46 @@
+import { BASE_URL } from "./constant.js";
+const FROMCURRENCY = document.getElementById("toConvertCurrency");
+const TOCURRENCY = document.getElementById("convertedCurrency");
+const INPUTFIELD = document.getElementById("toConvertAmount");
+const OUTFIELD = document.getElementById("convertedAmount");
+const BTN = document.getElementById("btn");
+
+export const getCurencyData = async () => {
+  let dataList = `<option value="" selected>Choose Currency</option>`;
+  let response = await fetch(
+    `${BASE_URL}/api/forex/v1/app-rate?page=1&per_page=10`
+  ).catch((error) => console.error(error));
+  const DATAS = await response.json();
+
+  DATAS.forEach((data) => {
+    if (data.unit == "100") {
+      dataList += `
+    <option value="${data.buy / 100}">${data.name} (${data.iso3}) </option>
+    `;
+    } else {
+      dataList += `
+        <option value="${data.buy}">${data.name} (${data.iso3})</option>
+        `;
+    }
+  });
+  FROMCURRENCY.innerHTML = dataList;
+  TOCURRENCY.innerHTML = dataList;
+};
+getCurencyData();
+
+BTN.onclick = () => {
+  let amtToConverted = INPUTFIELD.value;
+  let nepaliCurrencyFrom = FROMCURRENCY.value;
+  let nepaliCurrencyTo = TOCURRENCY.value;
+  if (
+    amtToConverted == "" ||
+    nepaliCurrencyFrom == "" ||
+    nepaliCurrencyTo == ""
+  ) {
+    alert("one or more values are missing!!!");
+  } else {
+    let equivalentAmt =
+      (amtToConverted * nepaliCurrencyFrom) / nepaliCurrencyTo;
+    OUTFIELD.value = equivalentAmt;
+  }
+};
